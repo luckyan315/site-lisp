23c3ef749aba2461d6c15f9a4257db08efc825b1
angl <luckyan315@qq.com>
2013-03-14 20:59:19 +0800

    update node test files

--- node/midware.js
+++ node/midware.js
@@ -1,17 +1,17 @@
 var Person = function(){
   this.name = 'lisi';
   this.print = function(text){
-    console.log(this.name + 'oriFun' + 'printed:' + text);
+    console.log(this.name + ' oriFun' + ' printed :' + text);
   };
 };
 
 var person = new Person();
 
 var midFun = function(obj){
-  var oldFun = obj.print();
+  var oldFun = obj.print;
   obj.print = function(text){
-    text = text + ' [midFun]';
-    obj.print(text);
+    text = text + '[midFun]';
+    oldFun(text);
   };
 };
 
@@ -23,6 +23,10 @@ var caller = function(obj){
   obj.print(obj.age);
 };
 
+
+console.log('mid fun print:\n');
 midFun(person);
+console.log('mid2 fun print:\n');
 mid2Fun(person);
+console.log('caller fun print:\n');
 caller(person);
