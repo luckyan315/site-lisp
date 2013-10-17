69f6aee2b167f0e421ecfc56b0da4247e32faa25
angl <luckyan315@qq.com>
2013-10-17 10:48:05 +0800

    update cscope files update

--- bin/updatecscopefiles.sh
+++ bin/updatecscopefiles.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-find . -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp" -o -name "*.cxx" > cscope.files
+find . -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp" -o -name "*.cxx" > cscope.files
 cscope -bkq -i cscope.files
 exit 0
