29e7bf84269ce0ded9fd7bb2f1561f2ee677fbeb
luckyan315 <luckyan315@qq.com>
2014-03-06 23:38:53 +0800

    up script skel

--- bin/skel.sh
+++ bin/skel.sh
@@ -7,13 +7,14 @@
 TEMP_FILE='./temp/*'
 
 #hook "ctrl + c"
-trap 'echo "hook ctrl + c" && rm -f $TEMP_FILE' INT
+#trap 'echo "hook ctrl + c" && rm -f $TEMP_FILE' INT
 
 #hook "app exit "
-trap 'echo "hook exit"' EXIT
+#trap 'echo "hook exit"' EXIT
 
 #help
 help() {
+    
     echo "Usage: `basename $0` <args>"
     echo ""
     echo "<description>"
@@ -21,6 +22,39 @@ help() {
     echo "  <options>"
     exit 1
 }
+
+getReturn() {
+    echo -n "Press return "
+    read ans
+    return 0
+}
+
+getConfirm() {
+    echo -n "Are you sure?"
+    while 1 ; do
+	read ans
+	case "$ans" in
+	    y | yes | Y | YES )
+		return 0
+		;;
+	    n | no | N | NO )
+		return 1
+		;;
+	    *)
+		echo "Please Enter yes(y) or no(n)!"
+		;;
+	esac
+    done
+}
+
+###############################################################################
+# main
+###############################################################################
+
+if [ $# -lt 1 ]; then
+    help
+    exit 1
+fi
 
 #parse args
 while getopts ":ht:" opt; do
